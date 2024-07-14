afe98040e29457b83c6507beacc25caf9198aa3f
tabspro <tabspro86@gmail.com>
2024-07-14 21:39:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
+st.write("""
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    Testing for different components
+    """
 )
